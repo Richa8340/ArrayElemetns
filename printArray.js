c094e763d6c66b291484444678f086beac170c15
Richa Rani <123743801+Richa8340@users.noreply.github.com>
2024-08-26 00:54:28 +0530

    Add files via upload

---
+++ printArray.js
@@ -0,0 +1,8 @@
+function printArrayElements(array) {
+    array.forEach(element => {
+        console.log(element);
+    });
+}
+
+// Usage example
+printArrayElements([1, 2, 3, 4, 5]); // Logs 1, 2, 3, 4, 5 each on a new line
